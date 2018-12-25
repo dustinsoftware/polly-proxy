639a547300e3193fdeb68d0cad738738a30e1103
Dustin Masters <dustin.masters@faithlife.com>
2018-12-24 20:15:57 -0800

    Fix __dirname in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,27 @@ const path = require('path');
 const NodemonPlugin = require('nodemon-webpack-plugin');
 
 module.exports = {
-	entry: './src/server-start.ts',
-	target: 'node',
-	mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				use: 'ts-loader',
-				exclude: /node_modules/
-			}
-		]
-	},
-	resolve: {
-		extensions: ['.tsx', '.ts', '.js']
-	},
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist')
-	},
-	plugins: [
-		new NodemonPlugin()
-	],
+  entry: './src/server-start.ts',
+  target: 'node',
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  node: {
+    __dirname: true,
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [new NodemonPlugin()],
 };
