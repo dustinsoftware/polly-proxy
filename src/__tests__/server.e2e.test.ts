6b238f50de9a7652f0b82c6ead5ce6a1ea9e7801
Dustin Masters <dustin.masters@faithlife.com>
2019-01-11 11:26:06 -0800

    Run prettier on the e2e tests

--- src/__tests__/server.e2e.test.ts
+++ src/__tests__/server.e2e.test.ts
@@ -9,11 +9,13 @@ describe('server e2e', () => {
 	let counter: number;
 
 	beforeAll(async () => {
-		server = stoppable(await express()
-			.get('/counter', (req, res) => {
-				res.json({ counter: counter++ });
-			})
-			.listen(3001));
+		server = stoppable(
+			await express()
+				.get('/counter', (req, res) => {
+					res.json({ counter: counter++ });
+				})
+				.listen(3001)
+		);
 	});
 
 	beforeEach(async () => {
@@ -23,8 +25,8 @@ describe('server e2e', () => {
 		pollyResponse = await fetch(
 			'http://localhost:3000/stop?testName=server-e2e',
 			{
-				method: 'POST',
-			},
+				method: 'POST'
+			}
 		).then(x => x.text());
 
 		expect(pollyResponse).toBe('');
@@ -40,46 +42,46 @@ describe('server e2e', () => {
 		const random = Math.random();
 		const response = await fetch(
 			`http://localhost:3000/addproxy?proxyPath=${encodeURIComponent(
-				`http://localhost:${(server.address() as AddressInfo).port}`,
+				`http://localhost:${(server.address() as AddressInfo).port}`
 			)}`,
 			{
-				method: 'POST',
-			},
+				method: 'POST'
+			}
 		).then(x => x.json());
 		expect(response.port).toBeGreaterThan(3000);
 
 		// should be a cache miss
 		expect(
 			(await fetch(
-				`http://localhost:${response.port}/counter?random=${random}`,
-			).then(x => x.json())).counter,
+				`http://localhost:${response.port}/counter?random=${random}`
+			).then(x => x.json())).counter
 		).toBe(5);
 
 		// should still be a cache miss
 		expect(
 			(await fetch(
-				`http://localhost:${response.port}/counter?random=${random}`,
-			).then(x => x.json())).counter,
+				`http://localhost:${response.port}/counter?random=${random}`
+			).then(x => x.json())).counter
 		).toBe(6);
 	});
 
 	it('record API calls', async () => {
 		const response = await fetch(
 			`http://localhost:3000/addproxy?proxyPath=${encodeURIComponent(
-				`http://localhost:${(server.address() as AddressInfo).port}`,
+				`http://localhost:${(server.address() as AddressInfo).port}`
 			)}`,
 			{
-				method: 'POST',
-			},
+				method: 'POST'
+			}
 		).then(x => x.json());
 		expect(response.port).toBeGreaterThan(3000);
 
 		let replayResponse: string;
 		replayResponse = await fetch(
-			'http://localhost:3000/replay?testName=server-e2e',
+			'http://localhost:3000/record?testName=server-e2e',
 			{
-				method: 'POST',
-			},
+				method: 'POST'
+			}
 		).then(x => x.text());
 
 		expect(replayResponse).toBe('');
@@ -87,22 +89,22 @@ describe('server e2e', () => {
 		let random = Math.random();
 
 		let cacheMissResponse = (await fetch(
-			`http://localhost:${response.port}/counter?random=${random}`,
+			`http://localhost:${response.port}/counter?random=${random}`
 		).then(x => x.json())).counter;
 
 		// should be a cache miss
 		expect(cacheMissResponse).toBe(5);
 
 		await fetch('http://localhost:3000/stop?testName=server-e2e').then(x =>
-			x.text(),
+			x.text()
 		);
 
 		// "rewind" the recording
 		replayResponse = await fetch(
 			'http://localhost:3000/replay?testName=server-e2e',
 			{
-				method: 'POST',
-			},
+				method: 'POST'
+			}
 		).then(x => x.text());
 
 		expect(replayResponse).toBe('');
@@ -110,8 +112,8 @@ describe('server e2e', () => {
 		// should be a cache hit
 		expect(
 			(await fetch(
-				`http://localhost:${response.port}/counter?random=${random}`,
-			).then(x => x.json())).counter,
+				`http://localhost:${response.port}/counter?random=${random}`
+			).then(x => x.json())).counter
 		).toBe(5);
 	});
 
