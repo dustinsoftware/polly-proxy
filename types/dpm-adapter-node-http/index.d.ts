550ca46acc1e6f148ca672983ceb0a1f4e769ef8
Dustin Masters <ceo@dustinsoftware.com>
2019-01-17 06:59:21 -0800

    Update pollyjs dependencies (and remove some hacks)

--- types/dpm-adapter-node-http/index.d.ts
+++
@@ -1,9 +0,0 @@
-import Adapter from '@pollyjs/adapter';
-
-interface PollyRequest {
-	promise: Promise<void>
-}
-
-export default class NodeHttpAdapter extends Adapter {
-	onRequest: (request: PollyRequest) => any;
-}
